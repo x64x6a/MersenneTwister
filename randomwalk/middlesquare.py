2a75bcbe7273cf715103949e868d45adb7b57b83
x64x6a <darekjens@gmail.com>
2016-04-13 23:25:28 -0500

    Resolved spacing issue

--- randomwalk/middlesquare.py
+++ randomwalk/middlesquare.py
@@ -1,45 +1,44 @@
 import math
 
-def middle(generator, seed, length):
-	#Seed2 will be the middle-square
-	seed2 = str(generator)
+def middle(generator, length):
+    #Seed will be the middle-square
+    seed = str(generator)
 
-	padd = float(len(str(generator)) - length)
+    padd = float(len(str(generator)) - length)
 
-	# If zero 
-	if not (padd % 2):
-		seed2 = seed2[:-int(padd / 2)]
-		seed2 = seed2[int(padd / 2):]
-	else:
-		seed2 = seed2[:-int(math.ceil(padd / 2))]
-		seed2 = seed2[int(math.floor(padd / 2)):]
+    # If zero 
+    if not (padd % 2):
+        seed = seed[:-int(padd / 2)]
+        seed = seed[int(padd / 2):]
+    else:
+        seed = seed[:-int(math.ceil(padd / 2))]
+        seed = seed[int(math.floor(padd / 2)):]
 
-	return int(seed2)
+    return int(seed)
 
 def random(seed):
-	length = len(str(seed))
-	while 1:
-		generator = seed ** 2
-		seed = middle(generator, seed, length)
-		yield int(str(seed).zfill(length))
-
+    length = len(str(seed))
+    while 1:
+        generator = seed ** 2
+        seed = middle(generator, length)
+        yield int(seed)
 
 if __name__ == '__main__':
-	#Get a Seed value from the user
-	seed = input("Enter a seed: ")
-	length = len(str(seed))
+    #Get a Seed value from the user
+    seed = input("Enter a seed: ")
+    length = len(str(seed))
+    print length
+    counter = 0
 
-	counter = 0
+    # Run for 5 rounds
+    while counter is not 6:
+        #Generate int for Seed2
+        generator = seed ** 2
 
-	# Run for 5 rounds
-	while counter is not 6:
-		#Generate int for Seed2
-		generator = seed ** 2
+        seed = middle(generator, length)
 
-		seed = middle(generator, seed)
+        print str(seed).zfill(length)
 
-		print str(seed).zfill(length)
-
-		counter = counter + 1
+        counter = counter + 1
 
 
--- randomwalk/randomwalk.py
+++ randomwalk/randomwalk.py
@@ -6,70 +6,71 @@ from os import urandom
 
 
 def repos(tmnt, widths, heights):
-	w,h = tmnt.pos()
-	neww,newh = w,h
+    w,h = tmnt.pos()
+    neww,newh = w,h
 
-	if w < widths[0]:
-		neww = widths[1] - (widths[0] - w)
-	elif w > widths[1]:
-		neww = widths[0] + (w - widths[1])
+    if w < widths[0]:
+        neww = widths[1] - (widths[0] - w)
+    elif w > widths[1]:
+        neww = widths[0] + (w - widths[1])
 
-	if h < heights[0]:
-		newh = heights[1] - (heights[0] - h)
-	elif h > heights[1]:
-		newh = heights[0] + (h - heights[1])
+    if h < heights[0]:
+        newh = heights[1] - (heights[0] - h)
+    elif h > heights[1]:
+        newh = heights[0] + (h - heights[1])
 
-	if neww != w or newh != h:
-		tmnt.penup()
-		tmnt.goto(neww,newh)
-		tmnt.pendown()
+    if neww != w or newh != h:
+        tmnt.penup()
+        tmnt.goto(neww,newh)
+        tmnt.pendown()
 
 if __name__ == '__main__':
-	SPEED = 0
-	DIST = 8
+    SPEED = 0
+    DIST = 8
 
-	wn = turtle.Screen()
-	#wn.bgcolor("#9C9C9C")
-	wn.bgcolor("black")
+    wn = turtle.Screen()
+    #wn.bgcolor("#9C9C9C")
+    wn.bgcolor("black")
 
-	turtle1 = turtle.Turtle()
-	turtle2 = turtle.Turtle()
-	turtle3 = turtle.Turtle()
+    turtle1 = turtle.Turtle()
+    turtle2 = turtle.Turtle()
+    turtle3 = turtle.Turtle()
 
-	turtle1.speed(SPEED)
-	turtle2.speed(SPEED)
-	turtle3.speed(SPEED)
+    turtle1.speed(SPEED)
+    turtle2.speed(SPEED)
+    turtle3.speed(SPEED)
 
-	turtle1.color("blue", "#B5BAFF")
-	turtle2.color("red", "#FFB5B5")
-	turtle3.color("white", "#3B3B3B")
+    turtle1.color("blue", "#B5BAFF")
+    turtle2.color("red", "#FFB5B5")
+    turtle3.color("white", "#3B3B3B")
 
-	width,height = turtle1.window_width(), turtle1.window_height()
-	widths = -(width/2),(width/2)
-	heights = -(height/2),(height/2)
+    width,height = turtle1.window_width(), turtle1.window_height()
+    widths = -(width/2),(width/2)
+    heights = -(height/2),(height/2)
 
-	# set random numbers
-	seed = int(urandom(2).encode('hex'),16) # change this to a better constant seed, sometimes msq becomes a constant number
-	rand = random.Random(seed)
-	msq = middlesquare.random(seed)
+    # set random numbers
+    seed = 31337
+    rand = random.Random(seed)
+    msq = middlesquare.random(seed)
 
-	while True:
-		a = rand.getrandbits(32)
-		b = int(urandom(4).encode('hex'), 16)
-		c = msq.next()
+    while True:
+        a = rand.getrandbits(32)
+        b = int(urandom(4).encode('hex'), 16)
+        c = msq.next()
+        print c
 
-		turtle1.setheading(a)
-		turtle1.forward(DIST)
+        turtle1.setheading(a)
+        turtle1.forward(DIST)
 
-		turtle2.setheading(b)
-		turtle2.forward(DIST)
+        turtle2.setheading(b)
+        turtle2.forward(DIST)
 
-		turtle3.setheading(c)
-		turtle3.forward(DIST)
+        turtle3.setheading(c)
+        turtle3.forward(DIST)
 
-		repos(turtle1, widths, heights)
-		repos(turtle2, widths, heights)
-		repos(turtle3, widths, heights)
+        repos(turtle1, widths, heights)
+        repos(turtle2, widths, heights)
+        repos(turtle3, widths, heights)
 
 
 
